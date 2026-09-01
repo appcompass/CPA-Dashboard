ea298e3ef353d02a3f8d5b9d8f148ee6829a810e
Harit Tarwani <112678530+HaritMT25@users.noreply.github.com>
2026-09-01 00:37:47 -0400

    Revert "Parse R sources as UTF-8"
    This reverts commit 389038080dad9488ecd49641ee93d3197ff4225a.

--- R/ui.R
+++ R/ui.R
@@ -1,22 +1,22 @@
 library(shiny.router)
 
 # layout
-source(file.path("R", "templates", "layout", "header_ui.R"), local = TRUE, encoding = "UTF-8")
-source(file.path("R", "templates", "layout", "footer_ui.R"), local = TRUE, encoding = "UTF-8")
-source(file.path("R", "templates", "layout", "main_ui.R"), local = TRUE, encoding = "UTF-8")
+source(file.path("R", "templates", "layout", "header_ui.R"), local = TRUE)
+source(file.path("R", "templates", "layout", "footer_ui.R"), local = TRUE)
+source(file.path("R", "templates", "layout", "main_ui.R"), local = TRUE)
 
 # pages
-source(file.path("R", "templates", "pages", "home_ui.R"), local = TRUE, encoding = "UTF-8")
-source(file.path("R", "templates", "pages", "about_ui.R"), local = TRUE, encoding = "UTF-8")
-source(file.path("R", "templates", "pages", "login_ui.R"), local = TRUE, encoding = "UTF-8")
-source(file.path("R", "templates", "pages", "organizations_ui.R"), local = TRUE, encoding = "UTF-8")
-source(file.path("R", "templates", "pages", "organization_details_ui.R"), local = TRUE, encoding = "UTF-8")
+source(file.path("R", "templates", "pages", "home_ui.R"), local = TRUE)
+source(file.path("R", "templates", "pages", "about_ui.R"), local = TRUE)
+source(file.path("R", "templates", "pages", "login_ui.R"), local = TRUE)
+source(file.path("R", "templates", "pages", "organizations_ui.R"), local = TRUE)
+source(file.path("R", "templates", "pages", "organization_details_ui.R"), local = TRUE)
 
 # components
-source(file.path("R", "templates", "components", "lang_change_nav_link_ui.R"), local = TRUE, encoding = "UTF-8")
-source(file.path("R", "templates", "components", "theme_toggle_nav_link_ui.R"), local = TRUE, encoding = "UTF-8")
-source(file.path("R", "templates", "components", "login_nav_link_ui.R"), local = TRUE, encoding = "UTF-8")
-source(file.path("R", "templates", "components", "organizations_list_ui.R"), local = TRUE, encoding = "UTF-8")
+source(file.path("R", "templates", "components", "lang_change_nav_link_ui.R"), local = TRUE)
+source(file.path("R", "templates", "components", "theme_toggle_nav_link_ui.R"), local = TRUE)
+source(file.path("R", "templates", "components", "login_nav_link_ui.R"), local = TRUE)
+source(file.path("R", "templates", "components", "organizations_list_ui.R"), local = TRUE)
 
 router <- router_ui(
   route("/", uiOutput("page_home")),
